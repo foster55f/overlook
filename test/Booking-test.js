e45d46c879f48cf3cb6a6476ec926a5af017ed19
“Foster <fosterataylor@yahoo.com>
2019-09-16 10:05:38 -0600

    Add Booking complete testing

--- test/Booking-test.js
+++ test/Booking-test.js
@@ -1,14 +1,12 @@
 import chai from 'chai';
 const expect = chai.expect;
 import Booking from '../src/Booking.js'
-import Room from '../src/Booking.js'
 let mockBookingData = require('../mock-data/mock-booking');
 let booking;
 
 before(() => {
   mockBookingData.forEach(booking => Booking.all.push(new Booking(booking)))
   booking = Booking.all[0]
-  
 });
 
 describe('Booking', () => {
@@ -17,15 +15,49 @@ describe('Booking', () => {
     expect(booking).to.be.an.instanceOf(Booking);
   })
 
-  it('should have a length of total bookings', () => {
-    booking.calculateRoomsAvailableTodaysDate();
-    expect(Booking.all.length).to.equal(15);
+  it('should display total rooms booked for a date', () => {
+    expect(Booking.findBookedRooms("2019/10/19")).to.eql([5]);
   })
 
-  it('should display total rooms available for todays date', () => {
-    booking.calculateRoomsAvailableTodaysDate();
-    expect(Booking.calculateRoomsAvailableTodaysDate()).to.equal(15);
+  it('should sort room numbers to a date', () => {
+    expect(Booking.occupiedRoomNumbersByDate()).to.eql({
+      '2019/10/19': [ 5 ],
+      '2019/10/30': [ 35 ],
+      '2019/09/01': [ 41, 44 ],
+      '2019/08/28': [ 13 ],
+      '2019/08/16': [ 23 ],
+      '2019/09/05': [ 26 ],
+      '2019/10/29': [ 34 ],
+      '2019/08/27': [ 37 ],
+      '2019/09/26': [ 36 ],
+      '2019/09/27': [ 12 ],
+      '2019/09/29': [ 34 ],
+      '2019/08/29': [ 45 ],
+      '2019/09/06': [ 19 ],
+      '2019/08/30': [ 13 ]
+    });
   })
+  
+it('should display the date rooms are least booked', () => {
+  console.log(Booking.findDateLeastRoomsBooked());
+  expect(Booking.findDateLeastRoomsBooked()).to.eql('2019/08/30');
+})
+  
+it('should display the date the most rooms are booked', () => {
+  console.log(Booking.findDateMostRoomsBooked());
+  expect(Booking.findDateMostRoomsBooked()).to.eql('2019/09/01');
+})
+  
+it('should display the date the most rooms are booked', () => {
+  console.log(Booking.findAllByCustomer(88));
+  expect(Booking.findAllByCustomer(88)).to.eql([{
+    "id": 4,
+    "date": "2019/08/28",
+    "roomNumber": 13,
+    "customerId": 88
+  }]);
+  })
+
 
 
 });
